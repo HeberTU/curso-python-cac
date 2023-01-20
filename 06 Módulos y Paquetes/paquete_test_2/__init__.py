932cd94db97ec4aa5b5ae491c22b24c5ee60c334
HeberTU <heber.trujillo@glovoapp.com>
2023-01-20 15:37:12 +0100

    feat: add package template lesson.

---
+++ 06 Módulos y Paquetes/paquete_test_2/__init__.py
@@ -0,0 +1,5 @@
+from paquete_test_2.base_datos import Conexion, BaseDatos
+from paquete_test_2.api_modelo import APIMod
+from paquete_test_2.estimador import Estimador
+
+__all__ = ["Conexion", "BaseDatos", "APIMod", "Estimador"]
