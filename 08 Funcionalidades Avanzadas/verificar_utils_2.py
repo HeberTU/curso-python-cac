8a58f5593d5a5e12b867665c2282e98ee57d32f5
HeberTU <heber.trujillo@glovoapp.com>
2023-01-24 07:41:36 +0100

    feat: add artifacts for lesson.

---
+++ 08 Funcionalidades Avanzadas/verificar_utils_2.py
@@ -0,0 +1,10 @@
+from utils import to_snake_case
+
+def verificar_utils():
+
+    print(to_snake_case(2) == "hola_mundo")
+    print(to_snake_case("Curso Python") == "curso_python")
+    print(to_snake_case("notebook") == "notebook")
+
+if __name__ == '__main__':
+    verificar_utils()
