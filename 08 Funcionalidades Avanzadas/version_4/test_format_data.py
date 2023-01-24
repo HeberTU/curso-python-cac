5db2f9c97d74e1480cc2efd2dd0db90e3bca0014
HeberTU <heber.trujillo@glovoapp.com>
2023-01-24 07:42:13 +0100

    feat: add artifacts for lesson.

---
+++ 08 Funcionalidades Avanzadas/version_4/test_format_data.py
@@ -0,0 +1,12 @@
+import pytest
+from utils import format_data_for_display, format_data_for_csv, Personas
+
+def test_format_data_for_display(personas: Personas):
+
+    assert format_data_for_display(personas) == [
+        "Heber Trujillo: Machine Learning Enineer",
+        "Montserrat Navarro: Data Enineer",
+    ]
+
+def test_format_data_for_csv(personas: Personas):
+    assert format_data_for_csv(personas) == "nombre,apellido,cargo\nHeber,Trujillo,Machine Learning Enineer\nMontserrat,Navarro,Data Enineer"
